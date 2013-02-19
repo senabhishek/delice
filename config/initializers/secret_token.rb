d48b9e684918b71961faf972a8882c434dbe49b5
Abhishek Sen <sen.abhishek@gmail.com>
2013-02-18 16:43:44 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Delice::Application.config.secret_token = 'ca38d6fa21784f163ce283cb6caa28cd0e7b23c96409a2845b4ad28c048431d371e1ac4b3af71476ab492e56aea345848958e1b45cb8f4567b339b2a1d879e8a'
